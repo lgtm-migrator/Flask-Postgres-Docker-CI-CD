9e4dda6ba76e787f503faacb3ac61bb86246a73f
Dennis W. Otugo <otugodennis@gmail.com>
2018-03-19 05:08:10 +0100

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -5,13 +5,10 @@ postgres_port=$2
 shift 2
 cmd="$@"
 
-# wait for the postgres docker to be running
 while ! pg_isready -h $postgres_host -p $postgres_port; do
-  >&2 echo "Postgres is unavailable - sleeping"
+  >&2 echo " Postgres is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "Postgres is up - executing command"
 
-# run the command
-# exec $cmd
